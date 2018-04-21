e091de0b80f0e5d58554d7bf15b41b2dbe4e00fc
zhuo <15110289101@163.com>
2018-04-21 21:14:57 +0800

    areaAx 1.0

--- js/westLoad.js
+++ js/westLoad.js
@@ -5,10 +5,18 @@ window.onload = function () {
         var westSide = document.getElementById("WestPublicTabList").getElementsByTagName("span");
         var westContainerWrap = document.getElementById("westContainerWrap").getElementsByTagName("section");
         tab(westSide,westContainerWrap);
-        // 西区侧边栏tab切换
+        // 西区展项控制tab切换
         var elCtrlTabTitWrap = document.getElementById("elCtrlTabTitWrap").getElementsByTagName("span");
         var westCtrlWrap = document.getElementById("westCtrlWrap").getElementsByTagName("section");
         tabCtrl(elCtrlTabTitWrap,westCtrlWrap);
+        // 清洁能源tab切换
+        var clearList = document.getElementById("clearList").getElementsByTagName("span");
+        var clearWrap = document.getElementById("clearWrap").getElementsByTagName("li");
+        tabCtrl(clearList,clearWrap);
+        // 清洁能源灯光控制tab切换
+        var clearMoreList = document.getElementById("clearMoreList").getElementsByTagName("span");
+        var clearMoreWrap = document.getElementById("clearMoreWrap").getElementsByTagName("li");
+        tabCtrl(clearMoreList,clearMoreWrap);
 
     // 播放器
 
@@ -30,6 +38,27 @@ window.onload = function () {
         // 西区企业文化 企业文化
             var movie06 = document.getElementById("movie06").getElementsByTagName("span");
             playCtrl(movie06);
+        // 清洁能源产业链 下游综述影片
+            var movie07 = document.getElementById("movie07").getElementsByTagName("span");
+            playCtrl(movie07);
+        // 清洁能源产业链 中游综述影片
+            var movie08 = document.getElementById("movie08").getElementsByTagName("span");
+            playCtrl(movie08);
+        // 清洁能源产业链 上游综述影片
+            var movie09 = document.getElementById("movie09").getElementsByTagName("span");
+            playCtrl(movie09);
+        // 清洁能源产业链 优势技术
+            var movie10 = document.getElementById("movie10").getElementsByTagName("span");
+            playCtrl(movie10);
+        // 清洁能源产业链 智慧建造
+            var movie11 = document.getElementById("movie11").getElementsByTagName("span");
+            playCtrl(movie11);
+        // 清洁能源产业链 智慧生产
+            var movie12 = document.getElementById("movie12").getElementsByTagName("span");
+            playCtrl(movie12);
+        // 清洁能源产业链 总结影片播放
+            var movie13 = document.getElementById("movie13").getElementsByTagName("span");
+            playCtrl(movie13);
 
     // 开关
 
@@ -134,8 +163,8 @@ window.onload = function () {
         // 西区企业文化 电视
             var doubleBtn32 = document.getElementById("doubleBtn32").getElementsByTagName("span");
             btnAct(doubleBtn32,btnActive);
-// 西区清洁能源 下游射灯
-    /*     var doubleBtn33 = document.getElementById("doubleBtn33").getElementsByTagName("span");
+        // 西区清洁能源 下游射灯
+         var doubleBtn33 = document.getElementById("doubleBtn33").getElementsByTagName("span");
          btnAct(doubleBtn33,btnActive);
          // 西区清洁能源 中游射灯
          var doubleBtn34 = document.getElementById("doubleBtn34").getElementsByTagName("span");
@@ -151,32 +180,107 @@ window.onload = function () {
          btnAct(doubleBtn37,btnActive);
          // 西区清洁能源 上游筒灯
          var doubleBtn38 = document.getElementById("doubleBtn38").getElementsByTagName("span");
-         btnAct(doubleBtn38,btnActive);*/
+         btnAct(doubleBtn38,btnActive);
+        // 西区清洁能源 设备控制 下游触摸屏
+        var doubleBtn38 = document.getElementById("doubleBtn39").getElementsByTagName("span");
+        btnAct(doubleBtn38,btnActive);
+        // 西区清洁能源 设备控制 中游触摸屏
+        var doubleBtn38 = document.getElementById("doubleBtn40").getElementsByTagName("span");
+        btnAct(doubleBtn38,btnActive);
+        // 西区清洁能源 设备控制 上游触摸屏
+        var doubleBtn38 = document.getElementById("doubleBtn41").getElementsByTagName("span");
+        btnAct(doubleBtn38,btnActive);
+        // 西区清洁能源 设备控制 LED屏
+        var doubleBtn38 = document.getElementById("doubleBtn42").getElementsByTagName("span");
+        btnAct(doubleBtn38,btnActive);
+        // 西区清洁能源 设备控制 渲染控制机
+        var doubleBtn38 = document.getElementById("doubleBtn43").getElementsByTagName("span");
+        btnAct(doubleBtn38,btnActive);
+        // 西区清洁能源 设备控制 渲染机1
+        var doubleBtn38 = document.getElementById("doubleBtn44").getElementsByTagName("span");
+        btnAct(doubleBtn38,btnActive);
+        // 西区清洁能源 设备控制 渲染机2
+        var doubleBtn38 = document.getElementById("doubleBtn45").getElementsByTagName("span");
+        btnAct(doubleBtn38,btnActive);
+        // 西区清洁能源 设备控制 渲染机3
+        var doubleBtn38 = document.getElementById("doubleBtn46").getElementsByTagName("span");
+        btnAct(doubleBtn38,btnActive);
+
+    // 音量调节
+
+        // 讲解系统
+        var westVolCtrl01 = document.getElementById("westVolCtrl01").getElementsByTagName("a");
+        var westVolLine01 = document.getElementById("westVolLine01").getElementsByTagName("span");
+        var westVol01 = new volCtrl(5);
+        vol(westVolCtrl01,westVolLine01,westVol01,westVol01.num);
+        // 序厅影片
+        var westVolCtrl02 = document.getElementById("westVolCtrl02").getElementsByTagName("a");
+        var westVolLine02 = document.getElementById("westVolLine02").getElementsByTagName("span");
+        var westVol02 = new volCtrl(5);
+        vol(westVolCtrl02,westVolLine02,westVol02,westVol02.num);
+        // 下游综述影片
+        var westVolCtrl04 = document.getElementById("westVolCtrl04").getElementsByTagName("a");
+        var westVolLine04 = document.getElementById("westVolLine04").getElementsByTagName("span");
+        var westVol04 = new volCtrl(5);
+        vol(westVolCtrl04,westVolLine04,westVol04,westVol04.num);
+        // 中游综述影片
+        var westVolCtrl05 = document.getElementById("westVolCtrl05").getElementsByTagName("a");
+        var westVolLine05 = document.getElementById("westVolLine05").getElementsByTagName("span");
+        var westVol05 = new volCtrl(5);
+        vol(westVolCtrl05,westVolLine05,westVol05,westVol05.num);
+        // 上游综述影片
+        var westVolCtrl06 = document.getElementById("westVolCtrl06").getElementsByTagName("a");
+        var westVolLine06 = document.getElementById("westVolLine06").getElementsByTagName("span");
+        var westVol06 = new volCtrl(5);
+        vol(westVolCtrl06,westVolLine06,westVol06,westVol06.num);
+        // 优势技术视频
+        var westVolCtrl07 = document.getElementById("westVolCtrl07").getElementsByTagName("a");
+        var westVolLine07 = document.getElementById("westVolLine07").getElementsByTagName("span");
+        var westVol07 = new volCtrl(5);
+        vol(westVolCtrl07,westVolLine07,westVol07,westVol07.num);
+        // 智慧建造视频
+         var westVolCtrl08 = document.getElementById("westVolCtrl08").getElementsByTagName("a");
+         var westVolLine08 = document.getElementById("westVolLine08").getElementsByTagName("span");
+         var westVol08 = new volCtrl(5);
+         vol(westVolCtrl08,westVolLine08,westVol08,westVol08.num);
+        // 智慧生产视频
+        var westVolCtrl09 = document.getElementById("westVolCtrl09").getElementsByTagName("a");
+        var westVolLine09 = document.getElementById("westVolLine09").getElementsByTagName("span");
+        var westVol09 = new volCtrl(5);
+        vol(westVolCtrl09,westVolLine09,westVol09,westVol09.num);
+        // 总结影片播放
+        var westVolCtrl10 = document.getElementById("westVolCtrl10").getElementsByTagName("a");
+        var westVolLine10 = document.getElementById("westVolLine10").getElementsByTagName("span");
+        var westVol10 = new volCtrl(5);
+        vol(westVolCtrl10,westVolLine10,westVol10,westVol10.num);
+        // 泛能装备
+        var westVolCtrl11 = document.getElementById("westVolCtrl11").getElementsByTagName("a");
+        var westVolLine11 = document.getElementById("westVolLine11").getElementsByTagName("span");
+        var westVol11 = new volCtrl(5);
+        vol(westVolCtrl11,westVolLine11,westVol11,westVol11.num);
+        // 泛能网影片
+        var westVolCtrl12 = document.getElementById("westVolCtrl12").getElementsByTagName("a");
+        var westVolLine12 = document.getElementById("westVolLine12").getElementsByTagName("span");
+        var westVol12 = new volCtrl(5);
+        vol(westVolCtrl12,westVolLine12,westVol12,westVol12.num);
+        // 前沿技术
+        var westVolCtrl13 = document.getElementById("westVolCtrl13").getElementsByTagName("a");
+        var westVolLine13 = document.getElementById("westVolLine13").getElementsByTagName("span");
+        var westVol13 = new volCtrl(5);
+        vol(westVolCtrl13,westVolLine13,westVol13,westVol13.num);
+        // 环境治理
+        var westVolCtrl14 = document.getElementById("westVolCtrl14").getElementsByTagName("a");
+        var westVolLine14 = document.getElementById("westVolLine14").getElementsByTagName("span");
+        var westVol14 = new volCtrl(5);
+        vol(westVolCtrl14,westVolLine14,westVol14,westVol14.num);
+        // 企业文化
+        var westVolCtrl15 = document.getElementById("westVolCtrl15").getElementsByTagName("a");
+        var westVolLine15 = document.getElementById("westVolLine15").getElementsByTagName("span");
+        var westVol15 = new volCtrl(5);
+        vol(westVolCtrl15,westVolLine15,westVol15,westVol15.num);
+
+
+
 };
 
-/*
-    // 音量控制部分
-        // 西区 讲解音量
-        var volNum1 = 5;
-        var volControl1 = document.getElementById("adjustWrap").getElementsByTagName("span");
-        adjustVol(volNum1,volControl1);
-        // 西区序厅 影片音量
-        var volNum2 = 5;
-        var volControl2 = document.getElementById("adjustWrap01").getElementsByTagName("span");
-        adjustVol(volNum2,volControl2);
-        // 西区物联时代 泛能装备音量
-        var volNum3 = 5;
-        var volControl3 = document.getElementById("adjustWrap02").getElementsByTagName("span");
-        adjustVol(volNum3,volControl3);
-        // 西区泛能网 平台影片音量
-        var volNum4 = 5;
-        var volControl4 = document.getElementById("adjustWrap03").getElementsByTagName("span");
-        adjustVol(volNum4,volControl4);
-        // 西区美丽生态 前沿技术影片音量
-        var volNum5 = 5;
-        var volControl5 = document.getElementById("adjustWrap04").getElementsByTagName("span");
-        adjustVol(volNum5,volControl5);
-        // 西区美丽生态 环境治理影片音量
-        var volNum6 = 5;
-        var volControl6 = document.getElementById("adjustWrap05").getElementsByTagName("span");
-        adjustVol(volNum6,volControl6);*/
+
